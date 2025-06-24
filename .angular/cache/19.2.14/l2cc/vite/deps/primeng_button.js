db7537f5a02122c8e4f2b46b4b7a9edc225e3537
paul-lautier <paul.lautier64@gmail.com>
2025-06-24 17:14:27 +0200

    add docs

---
+++ .angular/cache/19.2.14/l2cc/vite/deps/primeng_button.js
@@ -0,0 +1,29 @@
+import {
+  Button,
+  ButtonClasses,
+  ButtonDirective,
+  ButtonIcon,
+  ButtonLabel,
+  ButtonModule,
+  ButtonStyle
+} from "./chunk-YWM4ZQOY.js";
+import "./chunk-ZCWJQK5W.js";
+import "./chunk-PW5VDY5O.js";
+import "./chunk-5CLUPFUN.js";
+import "./chunk-JH3ACQ43.js";
+import "./chunk-RVWQFZF2.js";
+import "./chunk-QTQIARG7.js";
+import "./chunk-GBANFASH.js";
+import "./chunk-QZ2E34AJ.js";
+import "./chunk-5QXQOMPN.js";
+import "./chunk-P6U2JBMQ.js";
+import "./chunk-WDMUDEB6.js";
+export {
+  Button,
+  ButtonClasses,
+  ButtonDirective,
+  ButtonIcon,
+  ButtonLabel,
+  ButtonModule,
+  ButtonStyle
+};
